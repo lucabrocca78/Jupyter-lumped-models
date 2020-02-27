bb56fc81db73ca65a5aaeaa5b90e609863804f16
hckaraman <cagrkaraman@gmail.com>
2020-02-27 13:44:17 +0300

    upload2databse

--- Download/csv2psql.py
+++ Download/csv2psql.py
@@ -5,21 +5,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import select
 from sqlalchemy.types import String
 import datetime
+import pathlib
+
+path = pathlib.Path().absolute()
+os.chdir(os.path.join(path, 'Downloaded_data'))
+
+variable = ['2m_temperature', 'potential_evaporation', 'total_precipitation', 'snow_cover']
+tables = ['temperature','pot','pre''snow']
+var = variable[0]
+files = glob.glob('*'+ var + '*_final.csv')
 
 
-os.chdir('/media/cak/D/Datasets/ERA5_Land/Houtly2Daily/Temp')
-
-files = glob.glob('*_final.csv')
-
-
-
-for f in files:
+for i,f in enumerate(files):
     print("Process starterd at {} for year : {}".format(datetime.datetime.now().strftime('%H:%M:%S'), f))
-    columns = ['var', 'date', 'lat', 'lon', 'temp']
+    table = tables[i]
+    columns = ['var', 'date', 'lat', 'lon', table]
     df = pd.read_csv(f, skiprows=1, names=columns)
-    df.loc[df['temp'] == -32767.000, ['temp']] = np.nan
+    df.loc[df[table] == -32767.000, [table]] = np.nan
 
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+    engine = create_engine('postgresql://postgres:kalmanQs++@localhost:5999/postgres')
 
     result = engine.execute('SELECT lat,lon,index FROM '
                             '"cindex"')
@@ -29,6 +33,6 @@ for f in files:
 
     new_df = pd.merge(df, df_l, how='left', left_on=['lat', 'lon'], right_on=['lat', 'lon'])
     new_df.drop(['var', 'lat', 'lon'], axis=1, inplace=True)
-    new_df.to_sql('daily', engine, if_exists='append', index=False,
-                  dtype={"date": String(), "temp": String(), "index": String()})
+    new_df.to_sql(table, engine, if_exists='append', index=False,
+                  dtype={"date": String(), table: String(), "index": String()})
     print("Process ended at {} for year : {}".format(datetime.datetime.now().strftime('%H:%M:%S'), f))
