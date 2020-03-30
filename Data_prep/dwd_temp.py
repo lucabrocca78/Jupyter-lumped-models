a38216aba4edd23f9261db1132441dfd9ec14e56
Cagri <cagrkaraman@gmail.com>
2020-03-30 11:49:59 +0300

    DWD update

---
+++ Data_prep/dwd_temp.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+import time
+import datetime
+import schedule
+import os, glob
+import xarray as xr
+import pyowm
+import matplotlib.pyplot as plt
+import pandas as pd
+import sqlite3 as db
+
+folder = '/mnt/e/Test/T_2m'
+day = datetime.date.today()
+sql_data = str(day) + '.sqlite'
+os.chdir(folder)
+
+
+def get_forecast(lat, lon):
+    files = glob.glob('*.nc')
+    d = xr.open_dataset(files[0])
+    daily = d.sel(lon=lon, lat=lat, method='nearest')
+    df = daily.to_dataframe()
+    df = df.set_index(daily.time.values)
+    df = df.drop(['lon', 'lat'], axis=1)
+    # df.to_csv('test.csv')
+    return df
+
+
+def create_sql(sql_data, df_weather):
+    conn = db.connect(sql_data)
+    cur = conn.cursor()
+    cur.execute('''DROP TABLE IF EXISTS Result''')
+    df_weather.to_sql('Result', conn, if_exists='replace')
+
+
+def tosql(df_weather):
+    conn = db.connect(sql_data)
+    cur = conn.cursor()
+    # cur.execute('''DROP TABLE IF EXISTS Result''')
+    df_weather.to_sql('Result', conn, if_exists='append', index=True)  # - writes the pd.df to SQLIte DB
+    conn.commit()
+
+
+def get_data(df_weather):
+    owm = pyowm.OWM('9432d39621b9931ab44618f69f611f2d')
+    observation = owm.weather_at_id(323786)
+
+    w = observation.get_weather()
+
+    temp = (w.get_temperature('celsius')['temp'])
+    time = w.get_reference_time(timeformat='date') + datetime.timedelta(hours=3)
+    t = {'Date': str(time), 'temp_measured': temp}
+    temp = pd.DataFrame([t])
+    df_weather = df_weather.append(temp)
+    df_weather = df_weather.set_index('Date')
+    tosql(df_weather)
+    print('data inserted at {}'.format(str(datetime.datetime.now())))
+    # return df_weather
+
+
+def read_data():
+    conn = db.connect(sql_data)
+    cur = conn.cursor()
+    cur.execute("select * from Result")
+    results = cur.fetchall()
+    df_temp = pd.DataFrame(results, columns=['index', 'Date', 'Measured_Temp'])
+    df_temp = df_temp.set_index('Date')
+    df_temp = df_temp.drop(['index'], axis=1)
+    df_temp.index = pd.to_datetime(df_temp.index).strftime('%Y-%m-%d %H:%M:%S')
+    df_temp['Measured_Temp'] = df_temp['Measured_Temp'].astype(float)
+    df_temp.index = pd.to_datetime(df_temp.index)
+    return df_temp
+
+
+def plot_data(df, df_temp):
+    ax = df.plot(figsize=(18, 12))
+    df_temp.plot(ax=ax)
+    fig = plt.gcf()
+    name = datetime.datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
+    fig.savefig('{}.png'.format(name))
+
+
+lat = 39.8834
+lon = 32.9013
+df_weather = pd.DataFrame(columns=['Date', 'temp_measured'])
+df = get_forecast(lat, lon)
+
+create_sql(sql_data, df_weather)
+schedule.every(5).minutes.do(get_data, df_weather=df_weather)
+schedule.every(5).minutes.do(read_data)
+df_temp = read_data()
+schedule.every(5).minutes.do(plot_data, df=df, df_temp=df_temp)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- Data_prep/tiff2netcdf.py
+++ Data_prep/tiff2netcdf.py
@@ -1,23 +1,24 @@
 import os, glob
 
 folder = '/home/cak/Desktop/Datasets/SM2RAIN/10km'
+folder = '/mnt/e/Test/T_2m'
 
 os.chdir(folder)
 
-files = glob.glob('*.tif')
+files = glob.glob('*.grib2')
 
-for file in files:
-    name = file.split('.tif')[0][15:25]
-    date = name
-    infile = file
-    outfile = name + '.nc'
-
-    os.system("""gdal_translate -of netCDF -co "FORMAT=NC4" {} {}""".format(infile, outfile))
-    os.system("""ncrename -v Band1,{} {}""".format('tp', outfile))
-    os.system("""ncatted -O -a long_name,{},o,c,sm2rain {}""".format('tp', outfile))
-    os.system("""cdo settaxis,{},12:00:00,1day {} {}""".format(date, outfile, 't_' + outfile))
-    os.remove(outfile)
-    print(date)
+# for file in files:
+#     name = file.split('.tif')[0][15:25]
+#     date = name
+#     infile = file
+#     outfile = name + '.nc'
+#
+#     os.system("""gdal_translate -of netCDF -co "FORMAT=NC4" {} {}""".format(infile, outfile))
+#     os.system("""ncrename -v Band1,{} {}""".format('tp', outfile))
+#     os.system("""ncatted -O -a long_name,{},o,c,sm2rain {}""".format('tp', outfile))
+#     os.system("""cdo settaxis,{},12:00:00,1day {} {}""".format(date, outfile, 't_' + outfile))
+#     os.remove(outfile)
+#     print(date)
 
 # os.system("""cdo mergetime t*.nc {}.nc""".format(channel))
 # os.system("""mv {}.nc ./done""".format(channel))
@@ -34,11 +35,34 @@ for file in files:
 # os.chdir(folder)
 #
 # years = list(range(2009, 2021))
-years = list(range(2007, 2020))
-#
-for year in years:
-    files = glob.glob('t_' + str(year) + '*.nc')
+import datetime
 
-    code = """cdo mergetime t_{year}*.nc {year}.nc""".format(year=str(year))
+
+files = glob.glob('*.grib2')
+# _2020032909_000_1_T.grib2
+for file in files:
+
+    year =  int(file[43:47])
+    mount = int(file[47:49])
+    day = int(file[49:51])
+    run_time = int(file[51:53])
+    time_delta = int(file.split('_')[-2])
+    day = datetime.datetime(year, mount, day, run_time, 0, 0)
+    time = day + datetime.timedelta(hours=int(time_delta))
+    day = time.strftime('%Y-%m-%d')
+    hour = time.strftime('%H:%M:%S')
+    nc_out = file.split('.grib2')[0] + '.nc'
+    code = """grib_to_netcdf -o {} {}""".format(nc_out, file)
     os.system(code)
-    print(str(year) + ' done')
+    outfile = str(time_delta) + '_.nc'
+
+    code = """cdo shifttime,{}hour {} {}""".format(time_delta, nc_out, outfile)
+    os.system(code)
+
+#
+# for year in years:
+#     files = glob.glob('t_' + str(year) + '*.nc')
+#
+#     code = """cdo mergetime t_{year}*.nc {year}.nc""".format(year=str(year))
+#     os.system(code)
+#     print(str(year) + ' done')
