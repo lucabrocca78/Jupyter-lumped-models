2ba7b407e926094ee5a59a7cb633634d0629800b
Cagri <cagrkaraman@gmail.com>
2020-04-05 11:15:18 +0300

    gem update

--- Forecasts/get_gem.sh
+++ Forecasts/get_gem.sh
@@ -8,6 +8,7 @@ URL1='https://dd.weather.gc.ca/model_gem_global/15km/grib2/lat_lon/00/'
 URL2='/CMC_glb_TMP_TGL_2_latlon.15x.15_2020040500_P'
 URL3='.grib2'
 
+currentdate=`date +"%Y-%m-%d"`
 for i in {000..120..3}; do
   echo $i
   URL=${URL1}${i}${URL2}${i}${URL3}
@@ -16,7 +17,14 @@ for i in {000..120..3}; do
 done
 
 cdo mergetime *.grib2 merge.grib2
-wgrib2 merge.grib2 -netcdf temp.nc
-cdo subc,273.15 temp.nc CA_temp.nc
-cdo shaded,device="png",min=-5,max=25,lat_min=35,lat_max=45,lon_min=25,lon_max=45,step_freq=1 CA_temp.nc SC.png
-convert -delay 40 'SC.png_TMP_2maboveground.%d.png[0-41]' GFS_${currentdate}_temp.gif
+/home/cak/Desktop/software/grib2/wgrib2/wgrib2 merge.grib2 -netcdf temp.nc
+cdo subc,273.15 temp.nc GEM_$currentdate.nc
+cdo shaded,device="png",min=-5,max=25,lat_min=35,lat_max=45,lon_min=25,lon_max=45,step_freq=1 GEM_$currentdate.nc SC.png
+convert -delay 40 'SC.png_TMP_2maboveground.%d.png[0-41]' GEM_${currentdate}_temp.gif
+rm *.grib2
+rm *.png
+rm temp.nc
+tar -czvf GEM_$currentdate_bak.tar.gz GEM_$currentdate.nc
+mv GEM_$currentdate_bak.tar.gz GEM_$currentdate.nc ./Data
+mv GEM_${currentdate}_temp.gif ./GIF
+
