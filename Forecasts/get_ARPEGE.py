c6902d6110e295623de44a977bd75ddf1261f5a7
Cagri <cagrkaraman@gmail.com>
2020-04-06 05:47:51 +0300

    Measuremetns updated

--- Forecasts/get_ARPEGE.py
+++ Forecasts/get_ARPEGE.py
@@ -1,4 +1,4 @@
-#!usr/local/python3
+#!/usr/bin/python3
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
--- Forecasts/get_ARPEGE.sh
+++ Forecasts/get_ARPEGE.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-export WORKDIR=/home/cak/Desktop/Jupyter-lumped-models/Forecasts/ARPEGE
+export WORKDIR=/home/cak/Desktop/Jupyter-lumped-models/Forecasts
 cd ${WORKDIR}
 
-#./mnt/c/Users/cagri/Desktop/Jupyter-lumped-models/Forecasts/get_ARPEGE.py
+python3 get_ARPEGE.py
+
+export WORKDIR=/home/cak/Desktop/Jupyter-lumped-models/Forecasts/ARPEGE
+cd ${WORKDIR}
 
 currentdate=$(date +"%Y-%m-%d")
 
@@ -13,10 +16,13 @@ cdo subc,273.15 ARPEGE_${currentdate}_temp_K.nc ARPEGE_${currentdate}_temp_C.nc
 
 cdo shaded,device="png",min=-5,max=25,lat_min=35,lat_max=45,lon_min=25,lon_max=45,step_freq=1 -selvar,TMP_2maboveground ARPEGE_${currentdate}_temp_C.nc SC.png
 convert -delay 40 'SC.png_TMP_2maboveground.%d.png[0-115]' ARPEGE_${currentdate}_temp.gif
-mv ARPEGE_${currentdate}_temp_K.nc ARPEGE_${currentdate}_temp_C.nc ./Data
+mv ARPEGE_${currentdate}_temp_K.nc ARPEGE_${currentdate}_temp_C.nc ARPEGE_${currentdate}.nc ./Data
 mv ARPEGE_${currentdate}_temp.gif ./GIF
 
-#rm *.grib2
-#rm W_fr*
+rm *.grib2
+rm *.nc
+rm *.png
+cd ./Data
+tar -czvf ARPEGE_${currentdate}.tar.gz ARPEGE_${currentdate}.nc
 #rm t
 
