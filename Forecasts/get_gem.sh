cbbd835e83b72c05895933e78cb020764db98ad7
Cagri <cagrkaraman@gmail.com>
2020-04-06 14:33:24 +0300

    Measurement update

--- Forecasts/get_gem.sh
+++ Forecasts/get_gem.sh
@@ -4,18 +4,21 @@
 export WORKDIR=/home/cak/Desktop/Jupyter-lumped-models/Forecasts/GEM
 cd ${WORKDIR}
 
-URL1='https://dd.weather.gc.ca/model_gem_global/15km/grib2/lat_lon/00/'
-URL2='/CMC_glb_TMP_TGL_2_latlon.15x.15_2020040500_P'
-URL3='.grib2'
 
-currentdate=`date +"%Y-%m-%d"`
+currentdate=`date +"%Y%m%d"`
+URL1='https://dd.weather.gc.ca/model_gem_global/15km/grib2/lat_lon/00/'
+URL2='/CMC_glb_TMP_TGL_2_latlon.15x.15_'
+URL3='00_P'
+URL4='.grib2'
+
+
 for i in {000..120..3}; do
   echo $i
-  URL=${URL1}${i}${URL2}${i}${URL3}
+  URL=${URL1}${i}${URL2}${currentdate}${URL3}${i}${URL4}
   #echo $URL
   curl $URL -o CA${i}.grib2
 done
-
+currentdate=`date +"%Y-%m-%d"`
 cdo mergetime *.grib2 merge.grib2
 /home/cak/Desktop/software/grib2/wgrib2/wgrib2 merge.grib2 -netcdf temp.nc
 cdo subc,273.15 temp.nc GEM_$currentdate.nc
